d3cd01abfe9ba7eb43dc4db15bb89bc3cac5b023
Tanakorn Muwinitkul <tanakornfmu@gmail.com>
2022-08-25 11:07:00 +0700

    fix URL

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-  baseURL: 'https://fitplus-backend.vercel.app',
+  baseURL: 'https://fitplus-backend.vercel.app/',
   withCredentials: true, // เพื่อให้ส่ง cookie ได้
 });
